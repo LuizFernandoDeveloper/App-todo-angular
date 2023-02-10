a3af8372ab6dd834927cc611fd9e820b382d356d
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-02-10 14:41:07 -0300

    Array de task , ng class para controle do background , interface task

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,3 +1,4 @@
+import { TaskList } from './../../model/task-list';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [
+    {task:"Minha nova task um", checked: true},
+    {task:"Minha segunda tesk", checked: false },
+    {task:"Minha terceira tesk", checked: true }
 
+  ];
   constructor() { }
 
   ngOnInit(): void {

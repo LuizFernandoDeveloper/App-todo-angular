522f583955ea4449e3249fb340b974780015fa87
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-02-15 14:20:50 -0300

    Finalizing the project

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoInputAddItensComponent implements OnInit {
 
+  @Output() public emitItemTaskList = new EventEmitter();
+  public addItemTaskLid: string = "";
+  
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+
+  public submitItemTaskList() {
+    if(this.addItemTaskLid.trim().length >= 1){
+      this.emitItemTaskList.emit(this.addItemTaskLid.trim());
+      this.addItemTaskLid = "";
+    }
+  }
 }
